9a8988d6b5048ca243ba06a5cdbde7d3b1cf4772
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2021-08-19 16:12:52 -0300

    adiciona comentarios e status de privado ou publico para os artigos e comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create 
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
@@ -8,6 +8,6 @@ class CommentsController < ApplicationController
 
   private
     def  comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:commenter, :body, :status)
     end
 end
